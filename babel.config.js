b525f7e26fef827984e5e83a85733fed49c8c94e
Thiago de Souza <email@thiagodesouza.com.br>
2020-01-03 00:12:54 -0300

    chore: initial commit

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+module.exports = (api) => {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'babel-plugin-root-import',
+        {
+          rootPathSuffix: './src',
+          rootPathPrefix: '@/',
+        },
+      ],
+    ],
+  };
+};
